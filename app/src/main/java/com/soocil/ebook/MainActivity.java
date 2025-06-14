dd501ae08d869151f9f77c9a11d8b4971ff5074f
Soocil <63572085+sooocil@users.noreply.github.com>
2025-06-14 12:07:20 +0545

    Chore : Initial Setup and done with addbook page.

---
+++ app/src/main/java/com/soocil/ebook/MainActivity.java
@@ -0,0 +1,29 @@
+package com.soocil.ebook;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FloatingActionButton add_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        add_button = findViewById(R.id.add_button);
+        add_button.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, AddActivity.class);
+            startActivity(intent);
+        });
+    }
+}
